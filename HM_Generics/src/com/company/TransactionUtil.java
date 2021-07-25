0fcf04e3ea53521817d00b10a427ef48eddc943c
Egor <egor43585@bk.ru>
2021-07-25 23:25:52 +0300

    comments

--- HM_Generics/src/com/company/TransactionUtil.java
+++ HM_Generics/src/com/company/TransactionUtil.java
@@ -4,10 +4,10 @@ public class TransactionUtil {
 
     public static <T extends BaseWallet<Double>> void executeTransaction(T from, T to, double amount) throws NotEnoughMoneyException{
             if (from.getAmount() < amount) {
-                throw new NotEnoughMoneyException("Недостаточно средств");
+                throw new NotEnoughMoneyException("Недостаточно средств"); //хватает ли средств
             }
-            from.setAmount(from.getAmount() - amount);
-            to.setAmount(to.getAmount() + amount);
+            from.setAmount(from.getAmount() - amount);  //отнимаем от одного счёта
+            to.setAmount(to.getAmount() + amount);  //добавляем к другому счёту
 
         System.out.println("Тразакцитя удалась\n");
     }
